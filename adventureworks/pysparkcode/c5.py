568aa03f6b4487979a949858aaf33dddbfdcc3a4
JosephMachado <joseph.machado.professional@gmail.com>
2023-12-16 18:43:54 -0500

    y

--- adventureworks/pysparkcode/c5.py
+++ adventureworks/pysparkcode/c5.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import round, sum, col, count, date_add, lit, year
+from pyspark.sql.functions import round, sum, col, count, date_add, lit, year, avg
+from pyspark.sql import Window
 from pyspark.sql.types import (
     StructType,
     StructField,
@@ -19,6 +20,99 @@ class CodeRunner:
             "Window = A set of rows identified by values present in one or more column(s)"
         )
         print("=================================")
+        lineitem = spark.table("tpch.lineitem")
+        # Define the window specification
+        window_spec = Window.partitionBy("orderkey").orderBy("linenumber")
+
+        # Define the PySpark DataFrame API equivalent
+        result_df = (
+            lineitem.select(
+                "orderkey",
+                "linenumber",
+                "extendedprice",
+                round(sum("extendedprice").over(window_spec), 2).alias(
+                    "total_extendedprice"
+                ),
+            )
+            .orderBy("orderkey", "linenumber")
+            .limit(20)
+        )
+
+        # Show the result
+        result_df.show()
+
+    def run_exercise_53(self, spark):
+        print("=================================")
+        print("Defining a window function")
+        print("=================================")
+        nation = spark.table("tpch.nation")
+        customer = spark.table("tpch.customer")
+        orders = spark.table("tpch.orders")
+
+        result_df_1 = (
+            orders.join(customer, orders.custkey == customer.custkey)
+            .groupBy("customer.name", "orders.custkey", "orders.orderdate")
+            .agg(
+                col("customer.name").alias("customer_name"),
+                col("orders.orderdate").alias("orderdate"),
+                round(sum("orders.totalprice"), 2).alias("total_price"),
+                round(
+                    sum(sum("orders.totalprice")).over(
+                        Window.partitionBy("orders.custkey").orderBy("orders.orderdate")
+                    ),
+                    2,
+                ).alias("cumulative_sum_total_price"),
+            )
+            .orderBy("orders.custkey", "orders.orderdate")
+            .limit(20)
+        )
+
+        # Show the result for Query 1
+        result_df_1.show()
+
+        # Query 2
+        result_df_2 = (
+            orders.join(customer, orders.custkey == customer.custkey)
+            .join(nation, customer.nationkey == nation.nationkey)
+            .groupBy("nation.name", year("orders.orderdate"))
+            .agg(
+                col("nation.name").alias("nation_name"),
+                year("orders.orderdate").alias("order_year"),
+                round(sum("orders.totalprice") / 100000, 2).alias("total_price"),
+                round(
+                    avg(sum("orders.totalprice")).over(
+                        Window.partitionBy("nation.name").orderBy(
+                            year("orders.orderdate")
+                        )
+                    )
+                    / 100000,
+                    2,
+                ).alias("cumulative_sum_total_price"),
+            )
+            # .orderBy("nation_name", "order_year")
+            .limit(20)
+        )
+
+        # Show the result for Query 2
+        result_df_2.show()
+
+    def run_exercise_54(self, spark):
+        print("=================================")
+        print("Rank rows based on column(s) with Window ranking functions")
+        print("=================================")
+        pass
+
+    def run_exercise_55(self, spark):
+        print("=================================")
+        print("Compare column values across rows with Window value functions")
+        print("=================================")
+        pass
+
+    def run_exercise_56(self, spark):
+        print("=================================")
+        print("Choose rows to apply functions to within a window frame")
+        print("=================================")
+        pass
 
     def run_exercise(self, spark: SparkSession, exercise_num: int = 0):
         print("=================================")
--- adventureworks/run_code.py
+++ adventureworks/run_code.py
@@ -8,38 +8,43 @@ from pyspark.sql.types import (
     TimestampType,
 )
 
-from pysparkcode import c2, c3, c4
+from pysparkcode import c2, c3, c4, c5
+
 
 def run_spark_sql_code(section: str, spark: SparkSession):
-        pass
+    pass
+
 
 def run_pyspark_code(section: int, spark: SparkSession, exercise_num: int = 0):
-    print('=================================')
-    print('Running Pyspark Code')
-    print('=================================')
+    print("=================================")
+    print("Running Pyspark Code")
+    print("=================================")
     section_code_map = {
         2: c2.CodeRunner(),
-        3: c3.CodeRunner(),   
-        4: c4.CodeRunner()
-    } 
-    code_runner = section_code_map[section] 
+        3: c3.CodeRunner(),
+        4: c4.CodeRunner(),
+        5: c5.CodeRunner(),
+    }
+    code_runner = section_code_map[section]
     code_runner.run_exercise(spark, exercise_num)
 
-def run_book_code(section: str, spark: SparkSession, exercise_num: int = 0, runner: str = 'pyspark'):
-    if runner == 'pyspark':
+
+def run_book_code(
+    section: str, spark: SparkSession, exercise_num: int = 0, runner: str = "pyspark"
+):
+    if runner == "pyspark":
         run_pyspark_code(section, spark, exercise_num)
     else:
         run_spark_sql_code(section, spark, exercise_num)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Create a spark session
-    # Pass spark session with a variable that controls which chapter exercises to run 
+    # Pass spark session with a variable that controls which chapter exercises to run
     # The function should be able to accept the section number to run as well
-    # Have a function to run the spark sql code as well 
+    # Have a function to run the spark sql code as well
     spark = (
-        SparkSession.builder.appName("adventureworks")
-        .enableHiveSupport()
-        .getOrCreate()
+        SparkSession.builder.appName("adventureworks").enableHiveSupport().getOrCreate()
     )
     spark.sparkContext.setLogLevel("ERROR")
-    run_book_code(section=4, spark=spark, exercise_num=5)
+    run_book_code(section=5, spark=spark, exercise_num=3)
