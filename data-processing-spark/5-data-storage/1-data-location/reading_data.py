b1f2f2efa12bea4d82f4f7dbb4e0433e36b47396
josephkevinmachado <joseph.machado.services@gmail.com>
2024-03-21 12:02:15 -0400

    2024-03-21-12-02-data-storage-encode-and-sort

---
+++ data-processing-spark/5-data-storage/1-data-location/reading_data.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+
+def run_code(spark):
+    print("==========================================")
+    print("Reading data from external cloud storage system")
+    print("==========================================")
+    # Write a sample of orders data into S3 for this example
+    spark.sql("select * from tpch.orders limit 500").write.mode("overwrite").parquet("s3a://tpch/sample/orders.parquet")
+    print("Read Parquet data from S3 into a DataFrame")
+    df = spark.read.parquet("s3a://tpch/sample/orders.parquet")
+
+    print("Show the DataFrame schema and some sample data")
+    df.show()
+    
+    print("==========================================")
+    print("Reading data from local file system")
+    print("==========================================")
+    print("Get the local file location for our orders table")
+    spark.sql("DESCRIBE EXTENDED tpch.orders").show(n=100,truncate=False)
+    print("Serialized orders data")
+    spark.read.csv("file:/opt/spark/work-dir/spark-warehouse/tpch.db/orders").show()
+
+if __name__ == '__main__':
+    spark = (
+        SparkSession.builder.appName("adventureworks")
+        .enableHiveSupport()
+        .getOrCreate()
+    )
+    # Set the log level
+    spark.sparkContext.setLogLevel("ERROR")
+    run_code(spark=spark)
+    spark.stop
---
+++ data-processing-spark/5-data-storage/2-column-and-row-storage/column_storage.sql
@@ -0,0 +1,109 @@
+DROP SCHEMA IF EXISTS minio;
+
+CREATE SCHEMA minio;
+
+USE minio;
+
+DROP TABLE IF EXISTS lineitem_wo_encoding;
+
+CREATE TABLE lineitem_wo_encoding (
+    orderkey bigint,
+    partkey bigint,
+    suppkey bigint,
+    linenumber integer,
+    quantity double,
+    extendedprice double,
+    discount double,
+    tax double,
+    shipinstruct varchar(25),
+    shipmode varchar(10),
+    COMMENT varchar(44),
+    commitdate date,
+    linestatus varchar(1),
+    returnflag varchar(1),
+    shipdate date,
+    receiptdate date
+) USING CSV LOCATION 's3a://tpch/lineitem_wo_encoding/';
+
+INSERT INTO
+    minio.lineitem_wo_encoding
+SELECT
+    orderkey,
+    partkey,
+    suppkey,
+    linenumber,
+    quantity,
+    extendedprice,
+    discount,
+    tax,
+    shipinstruct,
+    shipmode,
+    COMMENT,
+    commitdate,
+    linestatus,
+    returnflag,
+    shipdate,
+    receiptdate
+FROM
+    tpch.lineitem;
+
+DROP TABLE IF EXISTS lineitem_w_encoding;
+
+CREATE TABLE lineitem_w_encoding (
+    orderkey bigint,
+    partkey bigint,
+    suppkey bigint,
+    linenumber integer,
+    quantity double,
+    extendedprice double,
+    discount double,
+    tax double,
+    shipinstruct varchar(25),
+    shipmode varchar(10),
+    COMMENT varchar(44),
+    commitdate date,
+    linestatus varchar(1),
+    returnflag varchar(1),
+    shipdate date,
+    receiptdate date
+) USING parquet LOCATION 's3a://tpch/lineitem_w_encoding/';
+
+INSERT INTO
+    lineitem_w_encoding
+SELECT
+    orderkey,
+    partkey,
+    suppkey,
+    linenumber,
+    quantity,
+    extendedprice,
+    discount,
+    tax,
+    shipinstruct,
+    shipmode,
+    COMMENT,
+    commitdate,
+    linestatus,
+    returnflag,
+    shipdate,
+    receiptdate
+FROM
+    tpch.lineitem;
+
+SELECT
+    suppkey,
+    sum(quantity) AS total_qty
+FROM
+    lineitem_w_encoding
+GROUP BY
+    suppkey;
+-- Time taken: 3.655 seconds, Fetched 10000 row(s)
+
+SELECT
+    suppkey,
+    sum(quantity) AS total_qty
+FROM
+    lineitem_wo_encoding
+GROUP BY
+    suppkey;
+-- Time taken: 11.924 seconds, Fetched 10001 row(s) 
