a634b81bf53728aa3b8573980d1f304a3eeb07d1
josephkevinmachado <joseph.machado.services@gmail.com>
2024-03-22 10:28:58 -0400

    2024-03-22-10-28-delta-table-frmt-code

--- data-processing-spark/5-data-storage/6-table-format/table_format_features.sql
+++ data-processing-spark/5-data-storage/6-table-format/table_format_features.sql
@@ -20,7 +20,11 @@ CREATE TABLE delta_lineitem(
     returnflag varchar(1),
     shipdate date,
     receiptdate date
-) USING DELTA LOCATION 's3a://tpch/delta_lineitem/';
+) USING DELTA LOCATION 's3a://tpch/delta_lineitem/' 
+TBLPROPERTIES (
+   'delta.columnMapping.mode' = 'name',
+   'delta.minReaderVersion' = '2',
+   'delta.minWriterVersion' = '5');
 
 INSERT INTO
     delta_lineitem
@@ -45,12 +49,55 @@ FROM
     tpch.lineitem;
 
 -- Look at versions
+DESCRIBE HISTORY delta_lineitem;
 
 -- Insert a few row
+-- Create a delta table
+
+INSERT INTO
+    delta_lineitem
+SELECT
+    orderkey,
+    partkey,
+    suppkey,
+    linenumber,
+    quantity,
+    extendedprice,
+    discount,
+    tax,
+    shipinstruct,
+    shipmode,
+    COMMENT,
+    commitdate,
+    linestatus,
+    returnflag,
+    shipdate,
+    receiptdate
+FROM
+    tpch.lineitem LIMIT 1000;
+
 
 -- look at versions now
+DESCRIBE HISTORY delta_lineitem;
+-- There would be 3 versions, 1 when the table was created, 2 when data was inserted the first time and 3 when data was inserted the second time
 
 -- go back to an older version
 ------------------------ TIME TRAVEL----------------------
+-- Current count
+select count(*) from delta_lineitem;
+-- 6,002,215
+select count(*) from delta_lineitem TIMESTAMP AS OF '2024-03-22 13:13:38';
+-- 6,001,215
+select count(*) from delta_lineitem VERSION AS OF 1;
+-- 6,001,215
 
+-- Schema evolution
+ALTER TABLE delta_lineitem  ADD COLUMN phonenumber STRING;
+ALTER TABLE delta_lineitem  DROP COLUMN COMMENT;
 
+DESCRIBE HISTORY delta_lineitem;
+
+-- Updates, Deletes and Merge into
+DELETE FROM delta_lineitem WHERE shipmode = 'TRUCK';
+UPDATE delta_lineitem SET shipmode = 'AIR' WHERE shipmode = 'REG AIR';
+-- MERGE INTO / UPSERT INTO especially useful for dimension tables
