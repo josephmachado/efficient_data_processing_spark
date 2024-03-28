df5e8108ca92b6dbe5e3e1fe84f503b7f0c39cf4
josephkevinmachado <joseph.machado.services@gmail.com>
2024-03-28 17:54:26 -0400

    2024-03-28-17-54-data-stats

---
+++ data-processing-spark/3-query-plan/query-plan-gen/check_data_stats.sql
@@ -0,0 +1,10 @@
+use tpch;
+desc extended lineitem; -- see data size
+
+analyze table lineitem compute statistics; -- scans table to identify number of rows
+desc extended lineitem; -- see table size and number of rows
+
+desc extended lineitem extendedprice; -- will be all NULLs
+analyze table lineitem compute statistics for all columns; -- scans table and identifies statistics for every column 
+desc extended lineitem extendedprice; -- information about min,max, col len, etc will be filled out
+`
