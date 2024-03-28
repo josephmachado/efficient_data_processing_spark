972d6bc4d547f6c02161131c778b6e83cd01acd1
josephkevinmachado <joseph.machado.services@gmail.com>
2024-03-28 17:13:00 -0400

    2024-03-28-17-13-explain-ui

---
+++ data-processing-spark/3-query-plan/what-is-query-plan/read-query-plan/explain_ui.py
@@ -0,0 +1,31 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, sum
+
+
+def run_code(spark):
+    spark.sql("USE tpch")
+    print("=======================================")
+    print("SELECT query")
+    print("=======================================")
+    result_df = (
+        spark.table("lineitem")
+        .alias("l")
+        .join(spark.table("orders").alias("o"), "orderkey")
+        .groupBy("o.orderkey")
+        .agg(
+            (sum(col("l.extendedprice") * (1 - col("l.discount"))))
+        )
+    )
+    result_df.show()
+
+if __name__ == '__main__':
+    spark = (
+        SparkSession.builder.appName("adventureworks")
+        .enableHiveSupport()
+        .getOrCreate()
+    )
+    # Set the log level
+    spark.sparkContext.setLogLevel("ERROR")
+    spark.conf.set("spark.sql.adaptive.enabled", "false")
+    run_code(spark=spark)
+    spark.stop
