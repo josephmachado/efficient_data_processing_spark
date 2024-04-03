8f218b6ce75affd93cf4defb6ba987fef7f0772f
josephkevinmachado <joseph.machado.services@gmail.com>
2024-04-03 18:34:06 -0400

    2024-04-03-18-34-windows-code

--- data-processing-spark/7-advanced-data-processing-patterns/2-window-functions/defining_windows.py
+++ data-processing-spark/7-advanced-data-processing-patterns/2-window-functions/defining_windows.py
@@ -9,6 +9,7 @@ def run_code(spark):
     lineitem = spark.table("tpch.lineitem")
     # Define the window specification
     window_spec = Window.partitionBy("orderkey").orderBy("linenumber")
+    total_extendedprice_prev_2_window = Window.partitionBy("orderkey").orderBy("linenumber").rowsBetween(Window.currentRow - 1, Window.currentRow)
 
     # Define the PySpark DataFrame API equivalent
     result_df = (
@@ -19,6 +20,7 @@ def run_code(spark):
             round(sum("extendedprice").over(window_spec), 2).alias(
                 "total_extendedprice"
             ),
+            round(sum("extendedprice").over(total_extendedprice_prev_2_window), 2).alias("total_extendedprice_prev_2_lineitems")
         )
         .orderBy("orderkey", "linenumber")
         .limit(20)
